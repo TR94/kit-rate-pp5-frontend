dc9f41cc1c0823aa781b2fc20e9a3e136a77f19f
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-01-21 14:05:37 +0000

    Initial set-up of KitRate react app for first deployment to Heroku

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!!</h1>
+      <h3>This will be the site for KitRate</h3>
+    </div>
+  );
+}
+
+export default App;
