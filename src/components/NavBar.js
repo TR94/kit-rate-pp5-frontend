f329a383aaff122f792220b1ba24dec5dd2fa1cf
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-02-07 14:56:55 +0000

    Add routes to navbar for logo, home, signin and signup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar, Container, Nav} from "react-bootstrap"
+import { NavLink } from "react-router-dom"
 import logo from "../assets/kitrate_logo.png"
 
 const NavBar = () => {
@@ -7,16 +8,18 @@ const NavBar = () => {
     // Based on React-Bootstrap NavBar component
         <Navbar expand="md" fixed="top">
             <Container>
-                <Navbar.Brand>
-                    <img src={logo} alt="logo" height="45"/>
-                    KitRate
-                </Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt="logo" height="45"/>
+                        KitRate
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-right">
-                        <Nav.Link><i className='fas fa-home'></i>Home</Nav.Link>
-                        <Nav.Link><i className='fas fa-sign-in-alt'></i>Sign-In</Nav.Link>
-                        <Nav.Link><i className='fas fa-user-plus'></i>Sign-Up</Nav.Link>
+                        <NavLink exact to="/"><i className='fas fa-home'></i>Home</NavLink>
+                        <NavLink to="/signin"><i className='fas fa-sign-in-alt'></i>Sign-In</NavLink>
+                        <NavLink to="/signup"><i className='fas fa-user-plus'></i>Sign-Up</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
