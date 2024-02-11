ea2340269b1a90b6a97d5c927a1614a5fb46f40c
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-02-11 11:47:14 +0000

    Update Navbar with new links and add Avatar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,11 +3,40 @@ import { Navbar, Container, Nav} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
 import logo from "../assets/kitrate_logo.png"
 import styles from "../styles/NavBar.module.css"
-import { useCurrentUser } from '../contexts/CurrentUserContext'
+import { useCurrentUser, useSetCurrentUser } from '../contexts/CurrentUserContext'
+import Avatar from './Avatar'
+import axios from 'axios'
 
 const NavBar = () => {
-    const currentUser = useCurrentUser()
-    const loggedInIcons = <>{currentUser?.username}</>
+    const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
+
+    const addProductIcon = (
+        <NavLink to="/products/create"><i className='far fa-plus-square'></i>Add Product</NavLink>
+    )
+
+    const handleSignOut = async () => {
+        try{
+            await axios.post("dj-rest-auth/logout/");
+            setCurrentUser(null);
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const loggedInIcons = <>
+        <NavLink to="/feed"><i className='fas fa-stream'></i>Feed</NavLink>
+        <NavLink to="/favourites"><i className='fas fa-heart'></i>Favourites</NavLink>
+        <NavLink 
+            to="/"
+            onClick={handleSignOut}
+        ><i className='fas fa-sign-out-alt'></i>Sign-Out</NavLink>
+        <NavLink 
+            to={`/profiles/${currentUser?.profile_id}`}
+        ><Avatar src={currentUser?.profile_image} text={currentUser?.profile_username} height={40}/></NavLink>
+        {/* fix the profile_username - see issue in project board */}
+    </>
+
     const loggedOutIcons = <>
         <NavLink to="/signin"><i className='fas fa-sign-in-alt'></i>Sign-In</NavLink>
         <NavLink to="/signup"><i className='fas fa-user-plus'></i>Sign-Up</NavLink>
@@ -23,6 +52,7 @@ const NavBar = () => {
                         KitRate
                     </Navbar.Brand>
                 </NavLink>
+                {currentUser && addProductIcon}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-right">
