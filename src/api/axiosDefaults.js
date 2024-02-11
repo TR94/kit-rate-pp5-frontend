0b60ed1d1e55d3bf6ca2805d12073bb9acfdc5e9
Tyrone Rutherford <tyrutherford@hotmail.co.uk>
2024-02-11 10:07:10 +0000

    Add axiosReq and axiosRes to CurrentUserContext for access token refresh

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,7 @@ import axios from "axios"
 axios.defaults.baseURL = 'https://kitrate-pp5-backend-47910aa247ff.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+// axios interceptors for request and response
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
